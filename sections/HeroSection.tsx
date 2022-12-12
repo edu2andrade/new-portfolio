3af4f0c1c3a7a9497f9bc4cb1c78057d667bc11c
Eduardo <edu2andrade@gmail.com>
2022-12-12 14:09:52 +0100

    Hero section implemented

--- sections/HeroSection.tsx
+++ sections/HeroSection.tsx
@@ -1,7 +1,36 @@
 'use-client'
 
+import Image from "next/image"
+import profilePic from '../public/profile_pic.webp'
+
+import { styles } from '../styles'
+import { FaArrowCircleDown } from 'react-icons/fa'
+
 export const HeroSection = () => {
   return (
-    <h1>HeroSection</h1>
+    <section className="h-screen">
+      <main className={`${styles.flexAround} ${styles.xPaddings} py-10 text-gray-300 z-10`}>
+        <div className="z-10">
+          <h1 className='font-extrabold text-8xl tracking-tighter leading-none'>
+            I am
+          </h1>
+          <span className='font-extrabold text-[200px] tracking-tighter leading-none bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-cyan-300'>
+            Edu.
+          </span>
+        </div>
+        <div className='w-96 h-96 relative'>
+          <Image
+            src={profilePic}
+            alt='Beautiful Smiling Face!'
+            width={380}
+            height={380}
+            priority
+          />
+        </div>
+      </main>
+      <div className={`${styles.flexCenter} animate-bounce text-gray-300`}>
+        <FaArrowCircleDown size={24}/>
+      </div>
+    </section>
   )
 }
