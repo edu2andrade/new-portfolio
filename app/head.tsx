db9646f53b1eac65021bc08feb076f6c6ffc7b9d
Eduardo <edu2andrade@gmail.com>
2022-12-17 12:58:32 +0100

    favicon added

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,8 @@ export default function Head() {
       <title>Eduardo Andrade | Web Developer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Web Developer mainly focused in React, React Native and Node" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/ico" href="/favicon.ico" />
     </>
   )
 }
