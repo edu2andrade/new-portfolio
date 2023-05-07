9d461833c3fb861d70bc79f5b930d8a37990a044
Eduardo <edu2andrade@gmail.com>
2023-05-07 11:51:26 +0200

    fix: head file back

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import { Inter } from '@next/font/google';
 import '../styles/globals.css';
+import Head from './head';
 const inter = Inter({ subsets: ['latin'] });
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Eduardo Andrade | Web Developer',
-  description:
-    'Freelance Web Developer mainly focused on React, React Native and Node',
-  metadata: {
-    name: 'facebook-domain-verification',
-    content: '0zv6eegwzzqr5tiribzg099utyko1p',
-  },
-};
 
 export default function RootLayout({
   children,
@@ -20,6 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <Head />
       <body>{children}</body>
     </html>
   );
